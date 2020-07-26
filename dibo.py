c6531df56539a67e7a72cef514a68e1abeb8c364
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-07-26 10:55:49 +0200

    Initial commit

---
+++ dibo.py
@@ -0,0 +1,42 @@
+import os
+from discord.ext.commands import Bot
+
+# Settings/globals
+TOKEN = os.environ['DIBO_TOKEN']
+SETTINGS = {
+    'prefix': '&',
+    'owner': os.environ['DIBO_OWNER'],
+    'owner_id': os.environ['DIBO_OWNER_ID']
+}
+BOT = Bot(command_prefix=SETTINGS['prefix'])
+
+
+@BOT.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(BOT))
+
+
+# Commands
+@BOT.command()
+async def echo(ctx, arg):
+    await ctx.send(arg)
+
+
+@BOT.command()
+async def info(ctx):
+    await ctx.send('Bot owner: {}'.format(SETTINGS['owner']))
+
+
+@BOT.command()
+async def kick(ctx):
+    if ctx.message.author.id == int(SETTINGS['owner_id']):
+        await ctx.send('Leaving guild.')
+        await ctx.guild.leave()
+
+
+def main():
+    BOT.run(TOKEN)
+
+
+if __name__ == "__main__":
+    main()
